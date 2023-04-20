e477f17c145d941ac4afbc771b2be6b8b790b037
rishi517 <riship517@gmail.com>
2023-04-19 22:20:11 -0400

    d

--- code/assignment.py
+++ code/assignment.py
@@ -1,101 +1,152 @@
 import tensorflow as tf
 import numpy as np
-from preprocess import get_data
+from preprocess import get_dataset
 from types import SimpleNamespace
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, input_shape):
+        super(Encoder, self).__init__()
+        self.conv2D1 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same', input_shape=input_shape)
+        self.bnorm = tf.keras.layers.BatchNormalization()
+        self.dropout = tf.keras.layers.Dropout(0.1)
+        self.conv2D2 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same')
 
 
-class MyTrigram(tf.keras.Model):
+    def call(self, x):
+        # Define the forward pass of the residual block
+        residual = x
+        residual = self.conv2D1(residual)
+        residual = self.bnorm(residual)
+        residual = self.dropout(residual)
+        
+        body = x
+        body = self.conv2D1(body)
+        body = self.bnorm(body)
+        body = self.dropout(body)
+        body = self.conv2D2(body)
+        body = self.bnorm(body)
+        x = tf.keras.layers.add([body, residual])
+        return x
 
-    def __init__(self, vocab_size, hidden_size=100, embed_size=64):
-        """
-        The Model class predicts the next words in a sequence.
-        : param vocab_size : The number of unique words in the data
-        : param hidden_size   : The size of your desired RNN
-        : param embed_size : The size of your latent embedding
-        """
+class ResidualBlock(tf.keras.layers.Layer):
+    def __init__(self, is_conv2d):
+        super(ResidualBlock, self).__init__()
+        self.conv2D = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same')
+        self.bnorm = tf.keras.layers.BatchNormalization()
+        self.dropout = tf.keras.layers.Dropout(0.1)
+        self.maxpool = tf.keras.layers.MaxPooling2D(2)
+        self.leaky = tf.keras.layers.LeakyReLU(alpha=0.1)
+        self.is_conv2d = is_conv2d
 
-        super().__init__()
+    def call(self, x):
+        # Define the forward pass of the residual block
+        residual = x
+        residual = self.conv2D(residual)
+        if not self.is_conv2d:
+            residual = self.maxpool(residual)
+        residual = self.bnorm(residual)
+        residual = self.dropout(residual)
+        
+        body = x
+        # leaky relu on body
+        body = self.leaky(body)
+        body = self.dropout(body)
+        if self.is_conv2d:
+            body = self.conv2D(body)
+            body = self.bnorm(body)
+        else:
+            body = self.maxpool(body)
+        body = self.leaky(body)
+        body = self.dropout(body)
+        body = self.conv2D(body)
+        body = self.bnorm(body)
+        body = self.dropout(body)
+        x = tf.keras.layers.add([body, residual])
+        return x
 
-        self.vocab_size = vocab_size
-        self.embed_size = embed_size
-        self.hidden_size = hidden_size
 
-        ## TODO: define your trainable variables and/or layers here. This should include an
-        ## embedding component, and any other variables/layers you require.
-        ## HINT: You may want to use tf.keras.layers.Embedding
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embed_size)
-        self.dense1 = tf.keras.layers.Dense(hidden_size, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(vocab_size, activation='softmax')
-        self.flatten = tf.keras.layers.Flatten()
+# class MyTrigram(tf.keras.Model):
+
+#     def __init__(self, vocab_size, hidden_size=100, embed_size=64):
+#         """
+#         The Model class predicts the next words in a sequence.
+#         : param vocab_size : The number of unique words in the data
+#         : param hidden_size   : The size of your desired RNN
+#         : param embed_size : The size of your latent embedding
+#         """
+
+#         super().__init__()
+
+#         self.vocab_size = vocab_size
+#         self.embed_size = embed_size
+#         self.hidden_size = hidden_size
+
+#         ## TODO: define your trainable variables and/or layers here. This should include an
+#         ## embedding component, and any other variables/layers you require.
+#         ## HINT: You may want to use tf.keras.layers.Embedding
+#         self.embedding = tf.keras.layers.Embedding(vocab_size, embed_size)
+#         self.dense1 = tf.keras.layers.Dense(hidden_size, activation='relu')
+#         self.dense2 = tf.keras.layers.Dense(vocab_size, activation='softmax')
+#         self.flatten = tf.keras.layers.Flatten()
 
         
 
 
-    def call(self, inputs):
-        """
-        You must use an embedding layer as the first layer of your network (i.e. tf.nn.embedding_lookup or tf.keras.layers.Embedding)
-        :param inputs: word ids of shape (batch_size, 2)
-        :return: logits: The batch element probabilities as a tensor of shape (batch_size, vocab_size)
-        """
-        inputs = self.embedding(inputs)
-        inputs = self.flatten(inputs)
-        inputs = self.dense1(inputs)
-        inputs = self.dense2(inputs)
+#     def call(self, inputs):
+#         """
+#         You must use an embedding layer as the first layer of your network (i.e. tf.nn.embedding_lookup or tf.keras.layers.Embedding)
+#         :param inputs: word ids of shape (batch_size, 2)
+#         :return: logits: The batch element probabilities as a tensor of shape (batch_size, vocab_size)
+#         """
+#         inputs = self.embedding(inputs)
+#         inputs = self.flatten(inputs)
+#         inputs = self.dense1(inputs)
+#         inputs = self.dense2(inputs)
 
 
-        return inputs
+#         return inputs
 
-    def generate_sentence(self, word1, word2, length, vocab):
-        """
-        Given initial 2 words, print out predicted sentence of targeted length.
-        (NOTE: you shouldn't need to make any changes to this function).
-        :param word1: string, first word
-        :param word2: string, second word
-        :param length: int, desired sentence length
-        :param vocab: dictionary, word to id mapping
-        """
-        reverse_vocab = {idx: word for word, idx in vocab.items()}
-        output_string = np.zeros((1, length), dtype=np.int32)
-        output_string[:, :2] = vocab[word1], vocab[word2]
+#     def generate_sentence(self, word1, word2, length, vocab):
+#         """
+#         Given initial 2 words, print out predicted sentence of targeted length.
+#         (NOTE: you shouldn't need to make any changes to this function).
+#         :param word1: string, first word
+#         :param word2: string, second word
+#         :param length: int, desired sentence length
+#         :param vocab: dictionary, word to id mapping
+#         """
+#         reverse_vocab = {idx: word for word, idx in vocab.items()}
+#         output_string = np.zeros((1, length), dtype=np.int32)
+#         output_string[:, :2] = vocab[word1], vocab[word2]
 
-        for end in range(2, length):
-            start = end - 2
-            output_string[:, end] = np.argmax(self(output_string[:, start:end]), axis=1)
-        text = [reverse_vocab[i] for i in list(output_string[0])]
+#         for end in range(2, length):
+#             start = end - 2
+#             output_string[:, end] = np.argmax(self(output_string[:, start:end]), axis=1)
+#         text = [reverse_vocab[i] for i in list(output_string[0])]
 
-        print(" ".join(text))
+#         print(" ".join(text))
 
 
 #########################################################################################
 
-def get_text_model(vocab):
-    '''
-    Tell our autograder how to train and test your model!
-    '''
+def get_model(input_shape):
 
-    ## TODO: Set up your implementation of the RNN
-
-
-    ## Optional: Feel free to change or add more arguments!
-    model = MyTrigram(len(vocab))
-
-    ## TODO: Define your own loss and metric for your optimizer
-    loss_metric = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-    
-    def perplexity(y_true, y_pred):
-        return tf.exp(tf.reduce_mean(loss_metric(y_true, y_pred)))
-    ## TODO: Compile your model using your choice of optimizer, loss, and metrics
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
-        loss=loss_metric, 
-        metrics=[perplexity],
-    )
-
-    return SimpleNamespace(
-        model = model,
-        epochs = 1,
-        batch_size = 100,
-    )
+    model = tf.keras.Sequential([
+        # Down-sampling blocks
+        Encoder(input_shape),
+        # Residual blocks
+        ResidualBlock(False),
+        ResidualBlock(False),
+        tf.keras.layers.UpSampling2D(2),
+        ResidualBlock(True),
+        tf.keras.layers.UpSampling2D(2),
+        ResidualBlock(True),
+        tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(1, 1, activation='sigmoid')
+        ])
+    return model
 
 
 #########################################################################################
@@ -104,43 +155,47 @@ def main():
 
     ## TODO: Pre-process and vectorize the data
     ##   HINT: You might be able to find this somewhere...
-    data_path = "../data"
+    file_pattern = '../data/next_day_wildfire_spread_train*'
     import preprocess
-    train_words_tokenized, test_words_tokenized, word_to_token_dict = preprocess.get_data(f"{data_path}/train.txt", f"{data_path}/test.txt")
+    side_length = 32 #length of the side of the square you select (so, e.g. pick 64 if you don't want any random cropping)
+    num_obs = 1000 #batch size
+    data = preprocess.get_dataset(
+      file_pattern,
+      data_size=64,
+      sample_size=side_length,
+      batch_size=num_obs,
+      num_in_channels=12,
+      compression_type=None,
+      clip_and_normalize=True,
+      clip_and_rescale=False,
+      random_crop=True,
+      center_crop=False)
+    inputs, labels = next(iter(data))
 
-    vocab = word_to_token_dict
-
-    def process_trigram_data(data):
-        X = np.array(data[:-1])
-        Y = np.array(data[2:])
-        X = np.column_stack((X[:-1], X[1:]))
-        return X, Y
-
-    X0, Y0 = process_trigram_data(train_words_tokenized)
-    X1, Y1 = process_trigram_data(test_words_tokenized)  
+    num_batches = 50
+    batch_size = num_obs // num_batches
 
 
+    print(inputs.shape)
+    print(labels.shape)
 
+    # fit the sequential model
+    model = get_model(inputs.shape[1:])
+    # compile model with weighted cross entropy loss
+    class_weights = tf.constant([1.0, 2.0])
 
-    # TODO: Get your model that you'd like to use
-    args = get_text_model(vocab)
+    # Create the loss function
+    def weighted_cross_entropy_with_logits(y_true, y_pred):
+        return tf.nn.weighted_cross_entropy_with_logits(labels=y_true, logits=y_pred, pos_weight=class_weights)
+    
+    # add an AUC pr metric
+    auc = tf.keras.metrics.AUC(curve='PR')
+    model.build(inputs.shape)
+    print(model.summary())
 
-    # TODO: Implement get_text_model to return the model that you want to use. 
-    args = get_text_model(vocab)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss=weighted_cross_entropy_with_logits, metrics=[auc])
+    model.fit(inputs, labels, epochs=10, batch_size=batch_size)
 
-    args.model.fit(
-        X0, Y0,
-        epochs=args.epochs, 
-        batch_size=args.batch_size,
-        validation_data=(X1, Y1)
-    )
-
-    ## Feel free to mess around with the word list to see the model try to generate sentences
-    words = 'speak to this brown deep learning student'.split()
-    for word1, word2 in zip(words[:-1], words[1:]):
-        if word1 not in vocab: print(f"{word1} not in vocabulary")
-        if word2 not in vocab: print(f"{word2} not in vocabulary")
-        else: args.model.generate_sentence(word1, word2, 20, vocab)
 
 if __name__ == '__main__':
     main()
